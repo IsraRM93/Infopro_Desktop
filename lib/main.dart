9c24fa1d17c4a26eec153a2b0cad06c1850fd8b9
Israel Rivera Mendoza <israelriveramendoza@outlook.com>
2025-05-29 11:26:33 -0600

    Actualizacion del Mein

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,28 @@ import 'providers/theme_provider.dart';
 import 'screens/login_screen.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const InfoproApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class InfoproApp extends StatelessWidget {
+  const InfoproApp({super.key});
+                                                                  
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Infopro Desktop',
-          theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
-          themeMode: themeProvider.themeMode, // Aplica el tema dinámico
-          home: const LoginScreen(),
-        );
-      },
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Infopro Desktop',
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            themeMode: themeProvider.themeMode,
+            home: const LoginScreen(),
+          );
+        },
+      ),
     );
   }
 }
