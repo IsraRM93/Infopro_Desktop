dc5dd7e90fadc50eabd1890a5558062e89d49cc3
Israel Rivera Mendoza <israelriveramendoza@outlook.com>
2025-06-19 12:11:10 -0600

    correxion 2 de archivos corruptos

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,21 @@
-allprojects {
+plugins {
+    // Apply any necessary plugins here
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+// Set custom build directory for root project
+buildDir = file("../../build")
+
+subprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-// Set custom build directory for root project
-rootProject.buildDir = file("../../build")
-
-subprojects {
-    // Set custom build directory for each subproject
-    buildDir = rootProject.file("../../build/${project.name}")
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+    evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
